2a5ae8b2889ce1806d3f8d0ccfd4276d0dea38d6
NourhaneAmir <92952125+NourhaneAmir@users.noreply.github.com>
2024-03-26 03:54:14 +0200

    Add files via upload

---
+++ Flutter App/Tarikhna/lib/modules/const.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Color primaryTextColor = const Color(0xFF414C6B);
+Color secondaryTextColor = const Color(0xFFE4979E);
+Color titleTextColor = Colors.white;
+Color contentTextColor = const Color(0xff868686);
+Color navigationColor = const Color(0xFF6751B5);
+Color meduim = const Color(0xFFE6C9FF);
+Color gradientStartColor = const Color(0xFFD7BEFF);
+
+Color gradientEndColor = const Color(0xFFF6E8D1);
+Color dotColor = const Color(0xFFA87DCF);
+//0xFFF4ECE1
+//0xFFF6E8D1
+//0xFFFCE1B4
+//0xFFD1EBFF
+//0xFFFCE1B4
---
+++ Flutter App/Tarikhna/lib/modules/main_card_statefull.dart
@@ -0,0 +1,145 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+import 'const.dart';
+
+class main_card_state extends StatefulWidget {
+  var question;
+  var answers;
+
+  main_card_state({Key? key, required this.question, required this.answers})
+      : super(key: key);
+
+  @override
+  State<main_card_state> createState() => _main_card_stateState();
+}
+
+class _main_card_stateState extends State<main_card_state> {
+  String choosenAnswer = 'none';
+
+  @override
+  Widget build(BuildContext context) {
+    var givenAnswers = widget.answers;
+    return SizedBox(
+      height: 400,
+      child: Card(
+        elevation: 8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(34.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.question,
+                style: TextStyle(
+                    color: primaryTextColor,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: 'Avenir',
+                    fontSize: 50),
+                textAlign: TextAlign.left,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Column(
+                children: [
+                  Row(children: [
+                    Expanded(
+                        child: RadioMenuButton(
+                      value: givenAnswers[0],
+                      groupValue: choosenAnswer,
+                      style: ButtonStyle(
+                        shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                        elevation: MaterialStatePropertyAll(2),
+                        backgroundColor: MaterialStatePropertyAll(meduim),
+                      ),
+                      onChanged: (val) {
+                        setState(() {
+                          choosenAnswer = val!;
+                        });
+                      },
+                      child: Text(givenAnswers[0]),
+                    )),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Expanded(
+                        child: RadioMenuButton(
+                      value: givenAnswers[1],
+                      groupValue: choosenAnswer,
+                      style: ButtonStyle(
+                        shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                        elevation: MaterialStatePropertyAll(2),
+                        backgroundColor: MaterialStatePropertyAll(meduim),
+                      ),
+                      onChanged: (val) {
+                        setState(() {
+                          choosenAnswer = val!;
+                        });
+                      },
+                      child: Text(givenAnswers[1]),
+                    ))
+                  ]),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: RadioMenuButton(
+                        value: givenAnswers[2],
+                        groupValue: choosenAnswer,
+                        style: ButtonStyle(
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                          elevation: MaterialStatePropertyAll(2),
+                          backgroundColor: MaterialStatePropertyAll(meduim),
+                        ),
+                        onChanged: (val) {
+                          setState(() {
+                            choosenAnswer = val!;
+                          });
+                        },
+                        child: Text(givenAnswers[2]),
+                      )),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Expanded(
+                          child: RadioMenuButton(
+                        value: givenAnswers[3],
+                        groupValue: choosenAnswer,
+                        style: ButtonStyle(
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                          elevation: MaterialStatePropertyAll(2),
+                          backgroundColor: MaterialStatePropertyAll(meduim),
+                        ),
+                        onChanged: (val) {
+                          setState(() {
+                            choosenAnswer = val!;
+                          });
+                        },
+                        child: Text(givenAnswers[3]),
+                      ))
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
