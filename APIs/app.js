f6bcf49da1ec492015a81ecf21d7b34ce5332fb5
Thomas Maged <thomasmaged26@gmail.com>
2024-03-26 18:01:34 +0200

    committing Quiz feature

--- APIs/app.js
+++ APIs/app.js
@@ -6,6 +6,7 @@ const userRouter = require('./User')
 const authMiddleware = require('./authentication/authMiddleware')
 const authRouter = require('./authentication')
 const lessonRouter =  require('./lesson/lessonRouter')
+const quizRouter = require('./quiz/quizRouter')
 const connectDb = require('./dbConnection')
 connectDb()
 
@@ -15,6 +16,8 @@ app.use('/AI_Summary', AIrouter)
 app.use('/auth', authRouter)
 app.use('/user', authMiddleware ,userRouter)
 app.use('/lesson',authMiddleware, lessonRouter)
+app.use('/quiz',authMiddleware ,quizRouter)
+
 
 
 
