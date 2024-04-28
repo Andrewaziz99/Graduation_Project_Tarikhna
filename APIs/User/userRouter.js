224e61fb0cb09c89de121c45d911f6a18d85e270
Thomas Maged <thomasmaged26@gmail.com>
2024-04-28 04:01:27 +0300

    update user in user router

--- APIs/User/userRouter.js
+++ APIs/User/userRouter.js
@@ -26,4 +26,32 @@ router.get('/', (req, res)=>{
 
 })
 
+router.put('/update', (req, res) => {
+    const currentUserData = {
+        _id: req.claims.id,
+        name: req.claims.name,
+        email: req.claims.email,
+        // password: req.claims.password,
+        year: req.claims.year
+    }
+    try {
+        userController.userUpdate(currentUserData, req.claims.id, req.body.newName, req.body.newEmail, req.body.oldPass, req.body.newPass, req.body.newYear, (err, data) => {
+            if (err) {
+                return res.send({ status: false, message: err, data: null })
+            }
+            req.claims.name = data.name
+            req.claims.email = data.email
+            req.claims.password = data.password
+            req.claims.year = data.year
+            return res.send({ status: true, message: "Data updated successfully", data: data })
+
+        })
+    } catch (error) {
+        return res.send({ status: false, message: "Error has occured, please try again after some time", data: null })
+
+    }
+
+
+})
+
 module.exports = router
