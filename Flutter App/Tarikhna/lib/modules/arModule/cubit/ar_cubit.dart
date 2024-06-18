f4bea601921d9a2b53fd9a0268e980bfdaf265e2
Andrew Aziz <andrewaziz097@gmail.com>
2024-06-18 16:17:33 +0300

    delete unnecessary files

--- Flutter App/Tarikhna/lib/modules/arModule/cubit/ar_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'ar_state.dart';
-
-class ArCubit extends Cubit<ArState> {
-  ArCubit() : super(ArInitial());
-  getARscreen() => emit(AROpenWidget());
-}
